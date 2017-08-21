8628dea49cb759d4b3c1abefd56e453704779f77
EdwinFarfan <edwin.farfan@sap.com>
2017-08-20 21:50:17 -0500

    equality for the strings

---
+++ basic/src/main/java/com/courses/java8/strings/StringEquals.java
@@ -0,0 +1,13 @@
+package com.courses.java8.strings;
+
+public class StringEquals {
+
+	public static void main(String[] args) {
+		System.out.println("abc".equals("ABC")); 
+		System.out.println("ABC".equals("ABC")); 
+		System.out.println("abc".equalsIgnoreCase("ABC")); 
+
+
+	}
+
+}
