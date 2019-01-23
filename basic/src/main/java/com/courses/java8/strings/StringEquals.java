47bfeccfb059514fc73899a8c33b3dde1ff95c09
Edwin Farfan <edwin.farfan@sap.com>
2019-01-23 15:29:59 -0500

    mostrar mensaje

--- basic/src/main/java/com/courses/java8/strings/StringEquals.java
+++ basic/src/main/java/com/courses/java8/strings/StringEquals.java
@@ -3,7 +3,7 @@ package com.courses.java8.strings;
 public class StringEquals {
 
 	public static void main(String[] args) {
-//		System.out.println("abc".equals("ABC")); 
+		System.out.println("abc".equals("ABC"));
 //		System.out.println("ABC".equals("ABC")); 
 //		System.out.println("abc".equalsIgnoreCase("ABC")); 
 
